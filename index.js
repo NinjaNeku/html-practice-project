c0d62eac8741253e15832edf1ae28304b1e3ba68
Aayush <aayushgautam2502@gmail.com>
2023-08-29 11:00:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstname = 'Aayush', lastname = 'Gautam';
+console.log(firstname,lastname);
+function greet(name){
+    console.log("hello",name)
+
+}
+greet('Aayush');
+
+function square(number){
+    return number*number;
+}
+let number = square(2);
+console.log(number);
